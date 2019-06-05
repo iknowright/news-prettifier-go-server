e673b5c3c538efbd9a65c4a915b0c0c6bba304fc
iknowright <changchaishi@gmail.com>
2019-06-05 11:25:38 +0800

    clear get news

--- app/model.go
+++ app/model.go
@@ -5,55 +5,6 @@ import (
 	"fmt"
 )
 
-type news struct {
-	ID    int     `json:"id"`
-	Link  string  `json:"link"`
-	ReturnLink string `json:"returnlink"`
-}
-
-func (n *news) getNews(db *sql.DB) error {
-	return db.QueryRow("SELECT link, returnlink FROM news WHERE id=$1", n.ID).Scan(&n.Link, &n.ReturnLink)
-}
-
-func (n *news) updateNews(db *sql.DB) error {
-	_, err := db.Exec("UPDATE news SET link=$1, returnlink=$2 WHERE id=$3", n.Link, n.ReturnLink, n.ID)
-	return err
-}
-
-func (n *news) deleteNews(db *sql.DB) error {
-	_, err := db.Exec("DELETE FROM news WHERE id=$1", n.ID)
-	return err
-}
-
-func (n *news) createNews(db *sql.DB) error {
-	// postgres doesn't return the last inserted ID so this is the workaround
-	err := db.QueryRow(
-		"INSERT INTO news(link, returnlink) VALUES($1, $2) RETURNING id",
-		n.Link, n.ReturnLink).Scan(&n.ID)
-	return err
-}
-
-func getMultipleNews(db *sql.DB, start, count int) ([]news, error) {
-	rows, err := db.Query("SELECT id, link, returnlink FROM news LIMIT $1 OFFSET $2", count, start)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	multipleNews := []news{}
-
-	for rows.Next() {
-		var n news
-		if err := rows.Scan(&n.ID, &n.Link, &n.ReturnLink); err != nil {
-			return nil, err
-		}
-		multipleNews = append(multipleNews, n)
-	}
-
-	return multipleNews, nil
-}
-
 type account struct {
 	Username  string  `json:"username"`
 	Password  string  `json:"password"`
